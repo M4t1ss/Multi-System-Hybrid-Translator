0a50ee164380d3e219369b41297cc8f6bd7562d2
Matīss Rikters <matiss_rikters@hotmail.com>
2016-03-21 22:04:56 +0200

    Fixed Bing token expiration
    Fixed Bing token expiration

--- API/bingTranslator.php
+++ API/bingTranslator.php
@@ -25,6 +25,28 @@ function translateWithBing($sourceLanguage, $targetLanguage, $textToTranslate){
 		foreach((array)$xmlObj[0] as $val){
 			$translatedStr = $val;
 		}
+		
+		if(strlen($translatedStr) == 0){			
+			echo "\nI guess the token expired\n";
+			echo "Let us try again...\n";
+			global $grantType, $scopeUrl, $BingClientID, $BingClientSecret, $authUrl; 
+			$authObj = new AccessTokenAuthentication();
+			$accessToken = $authObj->getTokens($grantType, $scopeUrl, $BingClientID, $BingClientSecret, $authUrl);
+			
+			$authHeader = "Authorization: Bearer ". $accessToken;
+			$contentType  = 'text/plain';
+			$category     = 'general';
+			$params = "text=".$textToTranslate."&to=".$targetLanguage."&from=".$sourceLanguage;
+			$translateUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?$params";
+			$translatorObj = new HTTPTranslator();
+			$curlResponse = $translatorObj->curlRequest($translateUrl, $authHeader);
+
+			$xmlObj = simplexml_load_string($curlResponse);
+			foreach((array)$xmlObj[0] as $val){
+				$translatedStr = $val;
+			}
+		}		
+		
 		return $translatedStr;
 	} catch (Exception $e) {
 		echo "Exception: " . $e->getMessage() . PHP_EOL;
